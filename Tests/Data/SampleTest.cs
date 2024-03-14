088ab6f6d6dcd4a7f4b49c42c96960172e0a0c76
Mateusz Kowalski <mati12k1@gmail.com>
2024-03-14 16:12:04 +0100

    ExampleTest

---
+++ Tests/Data/SampleTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Data
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void ExampleTest()
+        {
+            int firstNummber = 727;
+            int secondNumber = firstNummber + 1;
+
+            Assert.True(firstNummber < secondNumber);
+        }
+    }
+}
